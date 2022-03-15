ebb06156f1c3f6fe3a0dfbfdcebae7e836f8abd8
MahmoudBarak <mahmoud.ironman3000@amail.com>
2022-03-15 23:45:52 +0200

    dd

---
+++ lib/Screens/boot_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BootScreen extends StatefulWidget {
+  const BootScreen({Key? key}) : super(key: key);
+
+  @override
+  _BootScreenState createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/Screens/profile_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child: Center(
+          child: Column(
+
+            children: [
+
+          ],),
+        ),
+      ) ,
+    );
+  }
+}
